7e0dfeee2330075cc4444addddadacd0d0214b3e
Reuel Kulibaba <kulibabarj@gmail.com>
2023-02-12 17:45:54 -0300

    add js exercises

---
+++ exe.js
@@ -0,0 +1,32 @@
+const  
+
+// var a = 12.223;
+// a = 12.223;
+// a = "node";
+// a = false;
+var a = 2
+var b = prompt("Insira: ")
+
+for(let i = 1; i <= 10; i++){}
+
+
+
+// var apelido = 'Relampago marquinhos';
+// console.log('Nome: ' + a.nome, ' número: ', a.num, ' apelido: ', apelido);
+// console.log(`Nome: ${a.nome} número: ${a.num} apelido: ${apelido}`);
+
+
+
+
+
+
+/* var total = 0;
+for (let i = 0; i < 10; i++) {
+    total = somar(total, i);
+    // console.log('Valor i: ', i, ' total: ', total);
+} */
+// console.log("total: ", total);
+/* function somar(num, num1) {
+    return num + num1;
+} */
+
